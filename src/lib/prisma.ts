ad1f9a611c56ce288b4151057a0f5766ba8d7d61
suman Mishra <suman132j@gmail.com>
2026-03-28 06:53:26 +0530

    prisma push

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,14 +2,15 @@
 import 'server-only';
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
+
+const connectionString = `${process.env.DATABASE_URL}`
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
 
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient
 }
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-})
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({
   adapter,
